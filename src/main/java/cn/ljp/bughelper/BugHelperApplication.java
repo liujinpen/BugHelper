0f527aa7e406d2b77e854730e7462f54e87adda3
ljp <2271587828@qq.com>
2024-03-26 16:31:46 +0800

    初始化仓库

---
+++ src/main/java/cn/ljp/bughelper/BugHelperApplication.java
@@ -0,0 +1,13 @@
+package cn.ljp.bughelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BugHelperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/ljp/bughelper/BugHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.ljp.bughelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugHelperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
